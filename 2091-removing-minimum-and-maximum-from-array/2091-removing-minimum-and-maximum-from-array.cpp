629e455abd1e01c1f6864d90a1987e07d946f25d
Shorya0212 <sainishorya800@gmail.com>
2026-08-30 11:07:45 +0530

    Time: 7 ms (10.94%), Space: 91.9 MB (86.87%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -6,7 +6,7 @@ public:
         if(n == 1){
             return 1;
         }
-        for(int i = 1; i < n; i++){
+        for(int i = 0; i < n; i++){
             if(nums[i] > nums[mxi]){
                 mxi = i;
             }
